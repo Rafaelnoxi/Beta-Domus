d095c25477569191a0bac7b02ad2fcaeccb52f11
rafaelnoxi <rafatravain@gmail.com>
2025-10-20 00:27:33 -0300

    beta 1.1 - domus

--- depesas/Gerenciador_De_Despesas/urls.py
+++ depesas/Gerenciador_De_Despesas/urls.py
@@ -2,13 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name='home'),  # Tela inicial antes do login
-    path('login/', views.Login, name='login'),
-    path('criar_conta/', views.CriarConta, name='criar_conta'),
-    path('principal/', views.Principal, name='principal'),
-    path('logout/', views.LogoutView, name='logout'),
-    path('categorias/', views.listar_categorias, name='listar_categorias'),
-    path('despesa/adicionar/', views.adicionar_despesa, name='adicionar_despesa'),
-    path('despesas/listar/', views.listar_despesas, name='listar_despesas'),
-    path('despesa/excluir/<int:id>/', views.excluir_despesa, name='excluir_despesa'),
+    path('Login/', views.Login, name='Login'),
 ]
--- depesas/depesas/urls.py
+++ depesas/depesas/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('Gerenciador_De_Despesas.urls')),
     path('admin/', admin.site.urls),
-    path('', include('Gerenciador_De_Despesas.urls')),  # Inclui as URLs do app
 ]
